6486e29527220ce7f702863a4d0795cf1c6daa92
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2020-02-13 21:44:43 +0100

    Update post request with sequelize

--- routes/investments.js
+++ routes/investments.js
@@ -2,35 +2,42 @@ const router = require('express').Router();
 const db = require('../database');
 const Investment = require('../models/Investment');
 
-// router.post('/', (req, res) => {
-//     if(req.body == null) {
-//         res.status(400).send({message: "Body required"});
-//     } else if(req.body.updateDate == null) {
-//         res.status(400).send({message: "UpdateDate required"});
-//     } else if(req.body.name == null) {
-//         res.status(400).send({message: "Name required"});
-//     } else if(req.body.price == null) {
-//         res.status(400).send({message: "Price required"});
-//     } else {
-//         connection.query('insert into investments set ?', req.body, function(error, result, fields) {
-//             if(error) {
-//                 res.status(500).send({message: "error occured"});
-//             } else {
-//                 res.status(201).send({message: "investment added"});
-//             }
-//         });
-//     }
-// });
+router.post('/', (req, res) => {
+    let errors = [];
+    if (req.body.updateDate == null) {
+        errors.push({ errorMessage: 'UpdateDate required' });
+    }
+    if (req.body.name == null) {
+        errors.push({ errorMessage: 'Name required' });
+    }
+    if (req.body.price == null) {
+        errors.push({ errorMessage: 'Price required' });
+    }
+
+    if (errors.length > 0) {
+        res.status(400).send(errors);
+    } else {
+        Investment.create(req.body)
+            .then(() => {
+                console.log('investment created');
+                res.sendStatus(201);
+            })
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            })
+    }
+});
 
 router.get('/', (req, res) => {
     Investment.findAll()
-    .then(investments => {
-        res.status(200).send(investments);
-    })
-    .catch(err => {
-        console.log(err);
-        res.sendStatus(500);
-    });
+        .then(investments => {
+            res.status(200).send(investments);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = router;
