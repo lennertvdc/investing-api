1abdc760e29a96620c613443eeeba66f04896e16
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2020-02-24 12:00:00 +0100

    Use CORS for making requests to api

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const db = require('./database');
+
+// Using CORS for making requests from cross origin
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Using bodyParser for parsing incoming JSON requests to effective JSON
 server.use(bodyParser.json());
